ea586288cbcd6535267cf81c2a0766015ddb9e7b
Jonathan Glines <auntieNeo@gmail.com>
2011-03-06 17:05:44 -0700

    Fixed SDL linking issues.

--- src/soytracker/main.cpp
+++ src/soytracker/main.cpp
@@ -2,5 +2,12 @@
 
 int main(int argc, char **argv)
 {
+  AudioDriverManager *audioDriverManager = new AudioDriverManager();
+  if(audioDriverManager->getPreferredAudioDriver() == NULL)
+  {
+    delete audioDriverManager;
+    return 1;
+  }
+  delete audioDriverManager;
   return 0;
 }
