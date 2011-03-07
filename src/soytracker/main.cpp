df9553e70a03525ebdb82d5e91853ed57c9ee330
Jonathan Glines <auntieNeo@gmail.com>
2011-03-06 22:25:10 -0700

    Closer to getting milkyplay to work.

--- src/soytracker/main.cpp
+++ src/soytracker/main.cpp
@@ -7,21 +7,20 @@ int main(int argc, char **argv)
   if(argc != 2)
     return 1;
 
-  /*
-  AudioDriverManager *audioDriverManager = new AudioDriverManager();
-  if(audioDriverManager->getPreferredAudioDriver() == NULL)
-  {
-    delete audioDriverManager;
-    return 1;
-  }
-  */
+  MasterMixer *mixer = new MasterMixer(48000);
   XModule *module = new XModule();
   PlayerSTD *player = new PlayerSTD(48000);
   module->loadModule(argv[1]);
   player->startPlaying(module);
+  mixer->addDevice(player);
+  mixer->start();
+  while(mixer->isPlaying())
+  {
+    ;  // hurr, need to call something to get it to "tick"
+  }
   delete player;
   delete module;
-//  delete audioDriverManager;
+  delete mixer;
 
   
 
