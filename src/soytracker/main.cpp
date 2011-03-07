850d35a17a13c9bce663986e90dba06f936634ea
Jonathan Glines <auntieNeo@gmail.com>
2011-03-06 22:02:30 -0700

    Working on getting milkyplay to play things.

--- src/soytracker/main.cpp
+++ src/soytracker/main.cpp
@@ -1,13 +1,29 @@
-#include <MilkyPlay.h>
+#include "MilkyPlay.h"
+#include "PlayerSTD.h"
+#include "XMFile.h"
 
 int main(int argc, char **argv)
 {
+  if(argc != 2)
+    return 1;
+
+  /*
   AudioDriverManager *audioDriverManager = new AudioDriverManager();
   if(audioDriverManager->getPreferredAudioDriver() == NULL)
   {
     delete audioDriverManager;
     return 1;
   }
-  delete audioDriverManager;
+  */
+  XModule *module = new XModule();
+  PlayerSTD *player = new PlayerSTD(48000);
+  module->loadModule(argv[1]);
+  player->startPlaying(module);
+  delete player;
+  delete module;
+//  delete audioDriverManager;
+
+  
+
   return 0;
 }
