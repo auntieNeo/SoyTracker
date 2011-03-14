1187089ed918e570d371f30cab1049b4cb244d99
Jonathan Glines <auntieNeo@gmail.com>
2011-03-14 09:34:11 -0600

    Added copyright notices.

--- src/soytracker/moduleEditor.h
+++ src/soytracker/moduleEditor.h
@@ -1,3 +1,23 @@
+/***************************************************************************
+ *   Copyright 2011 Jonathan Glines.                                       *
+ *   auntieNeo@gmail.com                                                   *
+ *                                                                         *
+ *   This file is part of SoyTracker.                                      *
+ *                                                                         *
+ *   SoyTracker is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation, either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   SoyTracker is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with SoyTracker.  If not, see <http://www.gnu.org/licenses/>.   *
+ ***************************************************************************/
+
 #ifndef MODULE_EDITOR_H_
 #define MODULE_EDITOR_H_
 
