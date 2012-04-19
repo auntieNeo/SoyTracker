492256bc398f5000d0402c4bd4f33f3c264831f7
Jonathan Glines <auntieNeo@gmail.com>
2012-04-19 00:18:50 -0600

    Added linker option for the panels library. Added small panel test.

--- src/soytracker/main.cpp
+++ src/soytracker/main.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright 2011 Jonathan Glines.                                       *
+ *   Copyright 2012 Jonathan Glines.                                       *
  *   auntieNeo@gmail.com                                                   *
  *                                                                         *
  *   This file is part of SoyTracker.                                      *
@@ -26,6 +26,7 @@
 #include <locale.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <panel.h>
 
 #include <iostream>
 using namespace std;
@@ -34,20 +35,20 @@ using namespace SoyTracker;
 
 int main(int argc, char **argv)
 {
-  if(argc != 2)
-    return 1;
-
   setlocale(LC_ALL, "");
 
   // initilize screen and disable input line buffering and echoing
   // TODO: replace cbreak with raw in the future and catch control characters properly
-  initscr(); start_color(); use_default_colors(); cbreak(); noecho(); keypad(stdscr, TRUE);
+  initscr(); start_color(); use_default_colors(); cbreak(); noecho();
 
-  // some other settings recommended in the ncurses man page
+  /*
   nonl();
   intrflush(stdscr, false);
+  // enables reading of function keys and arrow keys
   keypad(stdscr, true);
+  // causes getch to be a non-blocking call
   nodelay(stdscr, true);
+  */
 
   // initilize the color pairs
   init_pair(CHANNEL_MARGIN_COLOR, -1, -1);
@@ -57,11 +58,23 @@ int main(int argc, char **argv)
   init_pair(EFFECT_NAME_COLOR, COLOR_MAGENTA, -1);
   init_pair(EFFECT_OPERAND_COLOR, COLOR_YELLOW, -1);
 
-  clear();
-  refresh();
+  // create a test panel
+  int lines = 10, cols = 20, y = 3, x = 5;
+  // create a window for the panel
+  WINDOW *testWindow = newwin(lines, cols, y, x);
+  // draw a box around the window
+  box(testWindow, 0, 0);
+  // attach a panel to the window
+  PANEL *testPanel = new_panel(testWindow);
 
-  // end curses mode
-  endwin();
+  // update panels, keeping them in the correct stacking order
+  update_panels();
+
+  // update the screen
+  doupdate();
+
+  getch();
+  endwin();  // end curses mode
 
   return 0;
 }
